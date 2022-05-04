f6dd6bbae453d85b5d7ec58ca5b3c219461684f9
AndrewTimv <76690013+AndrewTimv@users.noreply.github.com>
2022-05-04 23:36:18 +0300

    Add DB class

---
+++ database.py
@@ -0,0 +1,14 @@
+"""
+Class for work with database.
+Now work with sqlite3.
+"""
+
+import sqlite3
+
+
+class DBCheaters:
+    """
+    Class to work with db.
+    """
+    def __init__(self):
+        pass
