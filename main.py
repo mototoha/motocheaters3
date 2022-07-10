15a166ed17769b8e338a8113705a247fd7ec7bdf
AndrewTimv <76690013+AndrewTimv@users.noreply.github.com>
2022-07-10 18:19:18 +0300

    Refact

--- main.py
+++ main.py
@@ -215,7 +215,6 @@ def start_bot(db_filename: str, vk_token: str, cheaters_filename: str):
 
         await bot.answer_to_peer(answer_message, message.peer_id)
 
-
     # Отладочные команды. ---------------------------------------------------------------------------------------
     @bot.on.message(
         AdminUserRule(bot),
