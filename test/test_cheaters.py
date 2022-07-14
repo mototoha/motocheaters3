b3df0ae589c521c007a19c1056bb3f4d3e70503e
Андрей <antimoshkov@yandex.ru>
2022-07-14 18:33:24 +0300

    Добавил модуль для тестирования cheaters.py

---
+++ test/test_cheaters.py
@@ -0,0 +1,11 @@
+"""
+тесты для модуля cheaters.py
+"""
+import unittest
+import cheaters
+
+class TestCheater(unittest.TestCase):
+    def
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
